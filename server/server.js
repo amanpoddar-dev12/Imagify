a071685b81129e3776a438e740b1150e08a44e62
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-05-26 21:34:16 +0530

    Creating imagify

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
+// import inhan
+const PORT = process.env.PORT || 4000;
+import dotenv from "dotenv";
+import inhanceImageroutes from "./routes/inhanceImageRoutes.js";
+import imageRouter from "./routes/imageRoutes.js";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+await connectDB();
+// import imageRoutes from './routes/imageRoutes.js';
+
+app.use("/api/imageinhance", inhanceImageroutes);
+app.use("/api/user", userRouter);
+app.use("/api/image", imageRouter);
+app.get("/", (req, res) => res.send("API working"));
+app.listen(PORT, () => console.log("Server running on port" + PORT));
