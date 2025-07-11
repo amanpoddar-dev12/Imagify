0c06b2ab97090cffe9bd4d993fadb71725e2ac6f
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-07-12 00:16:33 +0530

    Update firebase.js

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -5,10 +5,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const serviceAccountPath = process.env.VITE_FIREBASE_ADMIN_KEY_PATH;
-const serviceAccount = JSON.parse(
-  fs.readFileSync(path.resolve(serviceAccountPath), "utf8")
-);
+const serviceAccountPath = path.resolve("./config/firebaseServiceKey.json");
+console.log(serviceAccountPath);
+if (!fs.existsSync(serviceAccountPath)) {
+  throw new Error(
+    "Firebase service key file not found at: " + serviceAccountPath
+  );
+}
+
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
