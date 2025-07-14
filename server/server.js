2fddbfa3e3cdaeaba68c6991868c074993efff0f
amanpoddar-dev12 <amanpoddar775@gmail.com>
2025-07-15 03:01:48 +0530

    fix cors issue in server.js

--- server/server.js
+++ server/server.js
@@ -13,9 +13,21 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 console.log(PORT);
 // Middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "imagify-ir9u5zlgq-amanpoddars-projects.vercel.app",
+];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", // Optional: replace with frontend URL in prod
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true, // enable if you're using cookies or auth headers
   })
 );
 
